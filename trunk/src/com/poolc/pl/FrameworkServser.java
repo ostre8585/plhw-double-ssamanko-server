6ae649ca69b13fec9db0245e0c0948bc1d5bd4a0
yoite@lycos.co.kr <yoite@lycos.co.kr@21db9aa5-b554-eddd-ae58-0c6d505bfc60>
2011-05-29 11:48:22 +0000

    initial commit

---
+++ trunk/src/com/poolc/pl/FrameworkServser.java
@@ -0,0 +1,26 @@
+package com.poolc.pl;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class FrameworkServser {
+	public static void main(String[] args) {
+		try {
+			ServerSocket server = new ServerSocket(10002);
+			
+			while(true) {
+				Socket sock = server.accept();
+				ServerThread serverThread = new ServerThread(sock);
+				serverThread.start();
+				
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+			
+		}
+			
+	
+	}
+
+}
---
+++ trunk/src/com/poolc/pl/ServerThread.java
@@ -0,0 +1,43 @@
+package com.poolc.pl;
+import java.io.*;
+import java.net.Socket;
+
+
+public class ServerThread extends Thread {
+	private Socket sock;
+	private PrintWriter pw;
+	private BufferedReader br;
+	
+	public ServerThread (Socket sock) {
+		this.sock = sock;
+		try {
+			pw = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()));
+			br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void run() {
+		try {
+			//do something
+
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(sock != null) {
+					pw.close();
+					br.close();
+					sock.close();
+				}
+			} catch (IOException ex) {
+				// TODO Auto-generated catch block
+				ex.printStackTrace();
+			}
+		}
+	}
+
+}
