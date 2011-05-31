62beb3d8cf261af17948178f87d345c336602337
yoite@lycos.co.kr <yoite@lycos.co.kr@21db9aa5-b554-eddd-ae58-0c6d505bfc60>
2011-05-31 20:24:48 +0000

    ???

--- src/com/poolc/pl/networkThread/FrameworkServser.java
+++ src/com/poolc/pl/networkThread/FrameworkServser.java
@@ -7,12 +7,14 @@ import java.net.Socket;
 public class FrameworkServser {
 	public static void main(String[] args) {
 		try {
-			ServerSocket server = new ServerSocket(10002);
+			ServerSocket server = new ServerSocket(2222);
 			
 			while(true) {
 				Socket sock = server.accept();
+				System.out.println("client accept\n");
 				ServerThread serverThread = new ServerThread(sock);
 				serverThread.start();
+				
 				
 			}
 		} catch (Exception e) {
--- src/com/poolc/pl/networkThread/ServerThread.java
+++ src/com/poolc/pl/networkThread/ServerThread.java
@@ -1,15 +1,18 @@
 package com.poolc.pl.networkThread;
 import java.io.*;
-import java.net.Socket;
+
+import java.net.*;
 
 
 public class ServerThread extends Thread {
 	private Socket sock;
 	private PrintWriter pw;
 	private BufferedReader br;
+	private FileWriter fw = null;
 	
 	public ServerThread (Socket sock) {
 		this.sock = sock;
+		
 		try {
 			pw = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()));
 			br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
@@ -17,27 +20,43 @@ public class ServerThread extends Thread {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
 	}
 	
 	public void run() {
+		 
 		try {
-			//do something
-
+			String str = null;
+			String filename = null;
+			while((str = br.readLine()) != null) {
+				
+//				if ( str == ~~) {						//mobile identifier
+//					filename = null;				 	//find or create filename
+//					fw = new FileWriter(filename);		//open file		
+//				}
+//				else {
+					fw.write(str);		
+//				}
+			}
 			
-		} catch (Exception e) {
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
+			
 			try {
-				if(sock != null) {
-					pw.close();
-					br.close();
-					sock.close();
-				}
-			} catch (IOException ex) {
+				pw.close();
+				br.close();
+				sock.close();
+			} catch (IOException e) {
 				// TODO Auto-generated catch block
-				ex.printStackTrace();
+				e.printStackTrace();
 			}
 		}
+			
+			
+		
 	}
 
 }
